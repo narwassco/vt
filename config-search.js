2ce0cb0cf4420bff516f78a6c48ace3656fea8d8
Ronoh2019 <60650620+Ronoh2019@users.noreply.github.com>
2024-10-09 16:24:14 +0300

    Update config-search.js
    Added details of 
    db: {
          user:process.env.db_user, - postgress
          password:process.env.db_password, - N@rw@ssc0
          host:process.env.db_host, Localhost
          port:process.env.db_port, 5432
          database:process.env.db_name, narwassco
        },

--- config-search.js
+++ config-search.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     db: {
-      user:process.env.db_user,
-      password:process.env.db_password,
-      host:process.env.db_host,
-      port:process.env.db_port,
+      user:process.env.postgres,
+      password:process.env.N@rw@ssc0,
+      host:process.env.localhost,
+      port:process.env.5432,
       database:'narwassco',
     },
     layers : [
