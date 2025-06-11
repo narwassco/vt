d046b3f22b00cef31cb6855713d697b664a54e58
Jin Igarashi <igarashi.jin@water-gis.com>
2025-06-11 20:53:52 +0900

    fix: fixed config.js for extraction, and deleted unnecessary old files. fixed CI to extract mbtiles to tiles folder

--- config-extract.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  ghpages:{
-    tiles: __dirname + '/public/tiles'
-  },
-  mbtiles: __dirname + '/data/narok.mbtiles',
-}
--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /tmp/src
-echo "db_user=${db_user}" > .env
-echo "db_password=${db_password}" > .env
-echo "db_host=${db_host}" > .env
-echo "db_port=${db_port}" > .env
-npm run create
